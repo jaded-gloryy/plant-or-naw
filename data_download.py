49538ee19b016f29948bcf63a0535dbb47c46ae9
Jade Burgin <jjr1945@rit.edu>
2022-11-01 12:06:32 -0400

    created file: function to download label and image data from db

---
+++ data_download.py
@@ -0,0 +1,17 @@
+#download data from Mongodb
+
+#provide image urls and data labels
+#read to dataframe
+
+def data_download(db_connection_string):
+
+    """
+    Input: string
+    Output: label | image_url
+
+    Args:
+        db_connection_string: connection to the database
+
+    """
+
+    return label | image_url
